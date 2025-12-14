40eafb216af404db7cff6569f995282666364292
Tanish6738 <123645393+Tanish6738@users.noreply.github.com>
2025-12-14 09:06:28 +0530

    baisc db done

---
+++ Backend/server.js
@@ -0,0 +1,18 @@
+import http from 'http';
+import app from './src/app.js';
+
+import { config } from 'dotenv';
+config();
+import connectDB from './src/config/db.js';
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+
+server.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
